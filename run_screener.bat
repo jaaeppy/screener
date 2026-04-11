1b9217a3b6463784a92460288fbd72c82d58a702
jaaeppy <jaaeppy@github.com>
2026-04-11 09:24:34 +0900

    update bat to include html files in push

--- run_screener.bat
+++ run_screener.bat
@@ -1,4 +1,7 @@
 @echo off
 cd /d C:\Users\dinky\screener
 C:\Users\dinky\AppData\Local\Programs\Python\Python311\python.exe screener.py
+git add screener_result.json index.html sector.html
+git commit -m "daily update"
+git push origin main
 pause
