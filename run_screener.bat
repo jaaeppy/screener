4df09a3c1187611d22ab72a8215cdeae1e85d2d5
jaaeppy <jaaeppy@github.com>
2026-04-17 06:16:12 +0900

    remove pause from bat, weekday-only schedule

--- run_screener.bat
+++ run_screener.bat
@@ -4,4 +4,3 @@ C:\Users\dinky\AppData\Local\Programs\Python\Python311\python.exe screener.py
 git add screener_result.json index.html sector.html
 git commit -m "daily update"
 git push origin main
-pause
